1a2b8b1b85612da1b16d5ac01234b7105bc5e5bc
David Merchan <davidmerchan10@gmail.com>
2024-11-14 19:37:47 -0500

    - Added validtion in url

--- presentation/src/main/java/com/davidmerchan/presentation/screen/articles/ArticlesListScreen.kt
+++ presentation/src/main/java/com/davidmerchan/presentation/screen/articles/ArticlesListScreen.kt
@@ -1,6 +1,5 @@
 package com.davidmerchan.presentation.screen.articles
 
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -30,8 +29,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -48,6 +49,9 @@ import com.davidmerchan.core.ui.ConnectionMessage
 import com.davidmerchan.core.ui.ErrorScreen
 import com.davidmerchan.core.ui.LoadingScreen
 import com.davidmerchan.core.ui.SwipeToDeleteBox
+import com.davidmerchan.core.utils.Toast
+import com.davidmerchan.core.utils.checkUrl
+import com.davidmerchan.core.utils.toast
 import com.davidmerchan.domain.entitie.Article
 import com.davidmerchan.domain.entitie.ArticleId
 import com.davidmerchan.presentation.R
@@ -74,6 +78,13 @@ fun ArticlesListScreen(
 
     val isConnected by remember(connectionState) {
         derivedStateOf { connectionState === NetworkConnectionState.Available }
+    }
+
+    var showMessageUrl by remember { mutableStateOf(false) }
+
+    if (showMessageUrl) {
+        context.toast(stringResource(R.string.article_no_url_error))
+        showMessageUrl = false
     }
 
     DeleteArticleSnackbar(
@@ -139,16 +150,18 @@ fun ArticlesListScreen(
                                     ArticlesUiEvent.DeleteArticle(id)
                                 )
                             },
-                            onGoToDetail = onGoToDetail
+                            onGoToDetail = { id, url ->
+                                if (url.checkUrl()) {
+                                    onGoToDetail(id, url)
+                                } else {
+                                    showMessageUrl = true
+                                }
+                            }
                         )
                     }
 
                     uiState.errorDeletedArticle != null -> {
-                        Toast.makeText(
-                            context,
-                            stringResource(R.string.article_deleted_error_message),
-                            Toast.LENGTH_LONG
-                        ).show()
+                        Toast(context, stringResource(R.string.article_deleted_error_message))
                     }
 
                     uiState.error != null -> ErrorScreen()
